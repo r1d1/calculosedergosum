23f148ab9234fc30b4ade7bbafb346e7f5d7935d
r1d1 <r1d1@users.noreply.github.com>
2014-05-16 13:48:25 +0200

    Color processing on image

--- src/tracking.cpp
+++ src/tracking.cpp
@@ -14,6 +14,8 @@ int main (int argc, char* argv[])
 {
 	IplImage* img = NULL;
 	const char* window_title = "Hello, OpenCV!"; 
+	//int windowsOffset = 1380;
+	int windowsOffset = 0;
 
 	CvCapture * capture = cvCreateCameraCapture( CV_CAP_ANY );
 
@@ -25,89 +27,84 @@ int main (int argc, char* argv[])
  
 	int selectedColor = atoi(argv[2]);
  
-/*	if (argc < 2)
-	{
-		fprintf (stderr, "usage: %s IMAGE\n", argv[0]);
-		return EXIT_FAILURE;
-	}*/
-
-//	img = cvLoadImage(argv[1], CV_LOAD_IMAGE_UNCHANGED);
- 
- /*	if (img == NULL)
-	{
-		fprintf (stderr, "couldn't open image file: %s\n", argv[1]);
-		return EXIT_FAILURE;
-	}
-  */
-/*	cout << "img metadata : " << img->nSize << " w : " << img->width << " h: " << img->height << " chan : " << img->nChannels << " sep : " << img->dataOrder << " imgsize : " << img->imageSize << endl;
-	cout << "img data : " << (int)img->imageData[0] << " " << (int)img->imageData[1] << " " << (int)img->imageData[2] << " " << (int)img->imageData[3] << " " << (int)img->imageData[4] << " " << (int)img->imageData[5] << " " << (int)img->imageData[6] << " " << endl;
-*/
 	IplImage * camflow;
 	IplImage * camflow_hsv;
-	IplImage * camflow_r;
-	IplImage * camflow_g;
-	IplImage * camflow_b;
+	IplImage * camflow_h;
+	IplImage * camflow_s;
+	IplImage * camflow_v;
+	IplImage * camflow_h_th;
+	IplImage * camflow_s_th;
+	IplImage * camflow_v_th;
+	IplImage * camflow_blobs;
+	IplImage * camflow_blobs_smooth;
 	
-	cvNamedWindow ("gray cam", CV_WINDOW_AUTOSIZE);  
-	cvNamedWindow ("cam flow", CV_WINDOW_AUTOSIZE);  
-	cvNamedWindow ("processing flow", CV_WINDOW_AUTOSIZE);
+//	cvNamedWindow ("gray cam", CV_WINDOW_AUTOSIZE);  
+//	cvNamedWindow ("cam flow", CV_WINDOW_AUTOSIZE);  
+//	cvNamedWindow ("processing flow", CV_WINDOW_AUTOSIZE);
 	
 	char key;
 	camflow = cvQueryFrame(capture);
 
-
 	camflow_hsv = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, camflow->nChannels);
-	camflow_r = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, 1);
-	camflow_g = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, 1);
-	camflow_b = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, 1);
+	camflow_h = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, 1);
+	camflow_s = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, 1);
+	camflow_v = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, 1);
+	camflow_h_th = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, 1);
+	camflow_s_th = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, 1);
+	camflow_v_th = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, 1);
+	camflow_blobs = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, 1);
+	camflow_blobs_smooth = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, 1);
 
 	while(key != 'q')
 	{
 		camflow = cvQueryFrame(capture);
 
 		std::cout << "Image channels (" << camflow->nChannels << ") ; channels organisation : " << camflow->dataOrder << std::endl;
-		std::cout << "Image channel R (" << camflow_r->nChannels << ") ; channels organisation : " << camflow_r->dataOrder << std::endl;
-		std::cout << "Image channel G (" << camflow_g->nChannels << ") ; channels organisation : " << camflow_g->dataOrder << std::endl;
-		std::cout << "Image channel B (" << camflow_b->nChannels << ") ; channels organisation : " << camflow_b->dataOrder << std::endl;
+		std::cout << "Image channel R (" << camflow_h->nChannels << ") ; channels organisation : " << camflow_h->dataOrder << std::endl;
+		std::cout << "Image channel G (" << camflow_s->nChannels << ") ; channels organisation : " << camflow_s->dataOrder << std::endl;
+		std::cout << "Image channel B (" << camflow_v->nChannels << ") ; channels organisation : " << camflow_v->dataOrder << std::endl;
 
 		cvCvtColor(camflow, camflow_hsv, CV_BGR2HSV);
-		cvSplit(camflow_hsv, camflow_r, camflow_g, camflow_b, NULL);
+		cvSplit(camflow_hsv, camflow_h, camflow_s, camflow_v, NULL);
 
-//		120, 255, 255
-	/*	double meanofpix=0;
-		double maxi=0;
-			
-		for(int i=0 ; i < graycam->imageSize ; i++)
-		{
-			diffcam->imageData[i] = ((graycam->imageData[i] - previousgraycam->imageData[i]) < 64) ? 0 : graycam->imageData[i] - previousgraycam->imageData[i];
-			previousgraycam->imageData[i] = graycam->imageData[i];
-		}
+//		cvThreshold(camflow_h, camflow_h_th, 115, 255, THRESH_BINARY);
+//		cvThreshold(camflow_h_th, camflow_h_th, 125, 255, THRESH_BINARY_INV);
+		cvThreshold(camflow_h, camflow_h_th, 170, 255, THRESH_BINARY);
+		cvThreshold(camflow_h_th, camflow_h_th, 10, 255, THRESH_BINARY_INV);
+		cvThreshold(camflow_s, camflow_s_th, 200, 255, THRESH_BINARY);
+//		cvThreshold(camflow_s, camflow_s_th, 5, 255, THRESH_BINARY_INV);
+		cvThreshold(camflow_v, camflow_v_th, 170, 255, THRESH_BINARY);
+//		cvThreshold(camflow_v, camflow_v_th, 5, 255, THRESH_BINARY_INV);
 
-		Mat smoothimage, originalimage(diffcam);
-		for(int i=1 ; i < MAX_KERNEL_LENGTH ; i=i+2 )
-			GaussianBlur(originalimage, smoothimage, Size(i,i), 0, 0);
-		
-		*smoothcam = smoothimage;
+		//camflow_blobs->imageData = (camflow_h_th->imageData + camflow_h_th->imageData + camflow_h_th->imageData) / 3.0;
+		IplImage * camflow_tmp = cvCreateImage(cvSize(camflow->width,camflow->height), camflow->depth, 1);
+		//cvAddWeighted(camflow_h_th, 1.0, camflow_s_th, 1.0, 0.0, camflow_tmp);
+		//cvThreshold(camflow_tmp, camflow_tmp, 200, 255, THRESH_BINARY);
+		//cvAdd(camflow_h_th, camflow_s_th, camflow_tmp);
+		//cvAddWeighted(camflow_tmp, 1.0, camflow_v_th, 1.0, 0.0, camflow_tmp);
+		//cvThreshold(camflow_tmp, camflow_blobs, 200, 255, THRESH_BINARY);
+		cvMul(camflow_h_th, camflow_s_th, camflow_tmp, 1.0/255.0);
+		cvMul(camflow_tmp, camflow_v_th, camflow_blobs, 1.0/255.0);
 
-		float maxima = 0;
-		int indexmaxi = 0;
+		Mat smoothed, detectionpic(camflow_blobs);
+		for(int i=1 ; i < 31 ; i=i+2)
+		{ GaussianBlur(detectionpic, smoothed, Size(i,i), 0, 0); }
+		*camflow_blobs_smooth = smoothed;
 
-		for(int i=0 ; i < smoothcam->imageSize ; i++)
-		{
-			maxima = (smoothcam->imageData[i] > maxima) ? smoothcam->imageData[i] : maxima;
-			indexmaxi = (smoothcam->imageData[i] > maxima) ? i : indexmaxi;
-		}
-	
-		smoothcam->imageData[indexmaxi] = 255;
-		cvShowImage ("gray cam", graycam);
-		cvShowImage ("cam flow", diffcam);
-		cvShowImage ("processing flow", smoothcam);
-		*/
-		cvShowImage ("input", camflow);
-		cvShowImage ("Red input", camflow_r);
-		cvShowImage ("Green input", camflow_g);
-		cvShowImage ("Blue input", camflow_b);
-		cvShowImage ("HSV", camflow_hsv);
+		// Compute center of the cluster, draw a point to get the coordinates
+
+	/*	cvShowImage("input", camflow);
+		cvMoveWindow("input",windowsOffset,10);
+		cvShowImage("H", camflow_h_th);
+		cvMoveWindow("H",windowsOffset,525);
+		cvShowImage("S", camflow_s_th);
+		cvMoveWindow("S",windowsOffset+650,525);
+		cvShowImage("V", camflow_v_th);
+		cvMoveWindow("V",windowsOffset+650*2,525);
+		cvShowImage("HSV", camflow_hsv);
+		cvMoveWindow("HSV",windowsOffset+650,10);
+	*/	cvShowImage("Out", camflow_blobs_smooth);
+	//	cvMoveWindow("Out",windowsOffset+650*2,10);
 		key = cvWaitKey(10);
 	}
 	cvDestroyAllWindows();
